7b42aac71e4afe3f59f438e4a56ed46c31d449a6
TrongNghia152004 <ttn.hdttb152004@gmail.com>
2023-05-05 16:22:28 +0700

    up

---
+++ excercise/ss5_form_router/contact-form/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {ContactForm} from "./component/contact_form";
+
+function App() {
+  return (
+    <div className="App">
+      <ContactForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ excercise/ss5_form_router/medical_declaration/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {MedicalDeclaration} from "./component/medical_declaration";
+
+function App() {
+    return (
+        <div className="App">
+            <MedicalDeclaration/>
+        </div>
+    );
+}
+
+export default App;
