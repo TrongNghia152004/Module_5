3115d1034406ece5afcf864793cd9113400575ec
TrongNghia152004 <ttn.hdttb152004@gmail.com>
2023-05-08 09:13:57 +0700

    up

--- cs/src/App.js
+++ cs/src/App.js
@@ -4,6 +4,10 @@ import {Footer} from "./component/footer";
 import {ServiceList} from "./component/facility/service_list";
 import {CustomerList} from "./component/customer/customer_list";
 import {Contract} from "./component/contract/contract";
+import {UpdateService} from "./component/facility/create_service";
+import {EditService} from "./component/facility/edit_service";
+import {CreateCustomer} from "./component/customer/create_customer";
+import {UpdateCustomer} from "./component/customer/update_customer";
 
 function App() {
     return (
@@ -11,7 +15,11 @@ function App() {
             <Header/>
             {/*<ServiceList/>*/}
             {/*<CustomerList/>*/}
-            <Contract/>
+            {/*<Contract/>*/}
+            {/*<UpdateService/>*/}
+            {/*<EditService/>*/}
+            {/*<CreateCustomer/>*/}
+            <UpdateCustomer/>
             <Footer/>
         </div>
     );
